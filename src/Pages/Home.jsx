c4a0cd162ed6be40657f556e7c58f16669ea9fab
Italo Marcelo <devfrontend2028study@gmail.com>
2025-03-15 18:14:58 -0300

    up home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
         </AppBar>
         <Box sx={{color: 'white',maxWidth: {lg: '700px'}, padding: {xs: '4em'}, position: 'relative', top: {xs: '14em', sm: '17em', md: '20em', lg: '10em'}, left: {lg: '5.5em'}, textAlign: {xs: 'center', lg: 'left'}}}>
            <Typography component='p' margin='1em 0'  display={{xs: 'block', lg: 'none'}}> <Typography component='strong' fontSize='2em'>dine</Typography>   </Typography>
-           <Typography component='h1' fontSize={{xs: '2em', lg: '5em'}} padding={{xs: '0 2em', lg: '0'}} marginBottom={{xs: '1em' ,lg: '0'}}>Exquisite dining since 1989</Typography>
+           <Typography component='h1' fontSize={{xs: '2em', lg: '5em'}} padding={{xs: '0 1em', lg: '0'}} marginBottom={{xs: '1em' ,lg: '0'}}>Exquisite dining since 1989</Typography>
            <Typography component='p' maxWidth={{lg: '350px'}} margin='1em 0' lineHeight='2em'>Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from 
            the comfort of our farmhouse.</Typography>
            <Button sx={{background: 'trasparent', fontWeight: '600', position: 'relative', top: '2em', border: 'none', outline: '1px solid white',color: '#fff', '&:hover': {background: 'white', color: '#000'}}}>Book a table</Button>
