82de41ea78dba0c98c61ab58e176cbf58fff02a8
Italo Marcelo <devfrontend2028study@gmail.com>
2025-03-15 16:12:54 -0300

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import {Stack, Box, AppBar} from '@mui/material'
+import logo from '../assets/images/logo.svg';
+export default function Home() {
+  return (
+     <Stack className='header-section' position='relative'>
+    
+     </Stack>
+  )
+}
+ 
