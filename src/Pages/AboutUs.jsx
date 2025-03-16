abd2372693d3927d5e2e18a1df91bcf77950899d
Italo Marcelo <devfrontend2028study@gmail.com>
2025-03-16 11:35:47 -0300

    up

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -12,7 +12,7 @@ export default function AboutUs() {
   return (
     <Stack component='section' className="Travel-plates" padding='1em' textAlign='center'
      >
-       <Stack component='article'  flexDirection={{lg: 'row'}} sx={{position: 'relative', top: '-5em'}}>
+       <Stack component='article'  flexDirection={{lg: 'row'}} sx={{position: 'relative', top: '-5em'}} justifyContent='center' alignItems='center'>
               <Box className='mobile'  >
               <img src={CheepImage1} alt="" />
               </Box>
@@ -22,17 +22,17 @@ export default function AboutUs() {
               <Box className='desktop' >
               <img src={CheepImage3} alt="" />
               </Box>
-          <Stack component='div' margin='1em 0'>
+          <Stack component='div' margin='1em 0' maxWidth='500px' >
               <Box className='line-image'position='relative'>
                  <Box className='bubble'>○</Box>
                  <Box className='line'></Box>
               </Box>
-              <Typography component='h2'><Typography component='strong' >Enjoyable place for all the family</Typography> </Typography>
-              <Typography component='p'>Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange 
+              <Typography component='h2' fontSize={{xs: '2em',md: '3em', lg: '3em'}} padding='0 1.5em' margin='1em 0'>Enjoyable place for all the family </Typography>
+              <Typography component='p' padding={{xs: '0 1.4em', md: '0 3.5em'}}  margin='.5em 0' >Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange 
               a tour of the farm before your meal.</Typography>
           </Stack>
        </Stack>
-       <Stack component='article' flexDirection={{lg: 'row'}} >
+       <Stack component='article' flexDirection={{lg: 'row-reverse'}} justifyContent='center' alignItems='center' >
           <Box className='mobile'>
               <img src={FoodPlate1} alt="" />
               </Box>
@@ -42,13 +42,13 @@ export default function AboutUs() {
               <Box className='desktop'>
               <img src={FoodPlate3} alt="" />
               </Box>
-          <Stack component='div' margin='1em 0'>
+          <Stack component='div'  margin='1em 0' maxWidth='500px'>
               <Box className='line-image' position='relative'>
               <Box className='bubble'>○</Box>
               <Box className='line'></Box>
               </Box>
-              <Typography component='h2'><Typography component='strong'>The most locally sourced food</Typography></Typography>
-              <Typography component='p'>All our ingredients come directly from our farm or local fishery. So you can be sure that you’re 
+              <Typography component='h2' fontSize={{xs: '2em',md: '3em', lg: '3em'}} padding='0 1.5em' margin='1em 0'>The most locally sourced food</Typography>
+              <Typography component='p' padding={{xs: '0 1.4em', md: '0 3.5em'}} margin='.5em 0' >All our ingredients come directly from our farm or local fishery. So you can be sure that you’re 
               eating the freshest, most sustainable food.</Typography>
           </Stack>
        </Stack>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,10 +6,10 @@ export default function Home() {
         <AppBar color='trasparent' position='static' sx={{display: {xs: 'none', lg: 'block'}, boxShadow: 'none', paddingTop: '3em', paddingLeft: '10em'}} >
            <img src={logo} alt=''/>
         </AppBar>
-        <Box sx={{color: 'white',maxWidth: {lg: '700px'}, padding: {xs: '4em'}, position: 'relative', top: {xs: '9em', sm: '17em', md: '20em', lg: '2.5em'}, left: {lg: '5.5em'}, textAlign: {xs: 'center', lg: 'left'}}}>
-           <Typography component='p' margin='1em 0'  display={{xs: 'block', lg: 'none'}}> <Typography component='strong' fontSize='2em'>dine</Typography>   </Typography>
-           <Typography component='h1' fontSize={{xs: '2em', lg: '5em'}} padding={{xs: '0 1em', lg: '0'}} marginBottom={{xs: '1em' ,lg: '0'}}>Exquisite dining since 1989</Typography>
-           <Typography component='p' maxWidth={{lg: '350px'}} margin='1em 0' lineHeight='2em'>Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from 
+        <Box sx={{color: 'white',maxWidth: {lg: '700px'}, padding: {xs: '4em'}, position: 'relative', top: {xs: '9em', sm: '17em', md: '25em', lg: '2.5em'}, left: {lg: '5.5em'}, textAlign: {xs: 'center', lg: 'left'}}}>
+           <Typography component='p' margin='1em 0'  display={{xs: 'block', lg: 'none'}}> <Typography component='strong' fontSize={{xs: '2em', md: '2.5em'}}>dine</Typography>   </Typography>
+           <Typography component='h1' fontSize={{xs: '2em', md: '4em', lg: '5em'}} padding={{xs: '0 1em',md : '0 4em', lg: '0'}} marginBottom={{xs: '1em' ,lg: '0'}}>Exquisite dining since 1989</Typography>
+           <Typography component='p' maxWidth={{lg: '500px'}} margin='1em 0' lineHeight='2em'>Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from 
            the comfort of our farmhouse.</Typography>
            <Button sx={{background: 'trasparent', width: '200px', height: '50px', fontWeight: '600', position: 'relative', top: '2em', border: 'none', outline: '1px solid white',color: '#fff', '&:hover': {background: 'white', color: '#000'}}}>Book a table</Button>
         </Box>
