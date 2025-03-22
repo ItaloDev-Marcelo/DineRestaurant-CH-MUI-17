8f8bead5ea474aa5b7b9f0f5d8dd3e173d7719e2
Italo Marcelo <devfrontend2028study@gmail.com>
2025-03-22 14:32:15 -0300

    up

--- src/Pages/nextPage/bookYourTable.jsx
+++ src/Pages/nextPage/bookYourTable.jsx
@@ -2,10 +2,9 @@ import {useEffect, useState} from 'react'
 import Footer  from '../Footer'
 import { AppBar, Stack, Box, Typography, Button} from '@mui/material'
 import Grid from '@mui/material/Grid2';
-
 import ScrollReveal from "scrollreveal";
 import Logo from '../../assets/images/logo.svg'
-
+import Line from '../../assets/images/patterns/pattern-lines.svg';
 import plus from '../../assets/images/icons/icon-plus.svg'
 import minus from '../../assets/images/icons/icon-minus.svg'
 
@@ -35,7 +34,7 @@ const [howManyPeople, setHowManyPeople] = useState(1)
          </AppBar>
         </Stack>
         
-          <Box  sx={{color: 'white',maxWidth: {lg: '700px'}, padding: {xs: '4em'}, position: 'relative', top: {xs: '4em', sm: '1em',  lg: '1.5em'}, left: {lg: '5.5em'}, textAlign: {xs: 'center', lg: 'left'}}} className='reveal'>
+          <Box  sx={{color: 'white',maxWidth: {lg: '700px'}, padding: {xs: '4em'}, position: 'relative', top: {xs: '4em', sm: '1em',  lg: '1em'}, left: {lg: '5.5em'}, textAlign: {xs: 'center', lg: 'left'}}} className='reveal'>
                    <Typography component='p' margin='1em 0'  display={{xs: 'block', md: 'none'}}> <Typography component='strong' fontSize={{xs: '2em', sm: '3.5em'}}>dine</Typography>   </Typography>
                    <Typography component='h1' fontSize={{xs: '2em', sm: '4em', lg: '5em'}} padding={{xs: '0 1em',sm : '0 1em', lg: '0'}} marginBottom={{xs: '1em' ,lg: '0'}} lineHeight={{xs:'1.2em', lg: '1.3em'}}>Reservations</Typography>
                    <Typography component='p' maxWidth={{lg: '400px'}} margin='1em 0' padding={{sm: '0 13em', lg: '0'}} fontSize={{md: '1.3em'}} lineHeight={{xs: '1.5em', lg: '1.7em'}} >We can’t wait to host you. If you have any special requirements please feel free to 
@@ -43,11 +42,13 @@ const [howManyPeople, setHowManyPeople] = useState(1)
                    <Button  onClick={() => Navigate('/DineHomePage')}   sx={{background: 'trasparent', width: '200px', height: '50px', fontWeight: '600', position: 'relative', top: '2em', border: 'none', outline: '1px solid white',color: '#fff', '&:hover': {background: 'white', color: '#000'}}}>Reserve Place</Button>
           </Box>
     </Stack>
-    <Stack  maxWidth={{xs: '90%', md: '450px'}} 
+    <Stack position='relative'>
+    <Stack  maxWidth={{xs: '90%', md: '450px'}}  
      backgroundColor='#fff' position='relative' top={{xs:'-15em' , md: '-5em', lg: '-25em'}}
      left={{xs: '1.5em', md:'17.5em', lg: '50em'}} padding='2em' height={{xs:'auto'}}
-      boxShadow='8px 8px 5px -6px rgba(0,0,0,0.41)' className='reveal'>
+      boxShadow='8px 8px 5px -6px rgba(0,0,0,0.41)' className='reveal form' zIndex='4'>
       
+       
       <Grid container>
         
       <Grid size={12}  className='area-1' >
@@ -119,6 +120,10 @@ const [howManyPeople, setHowManyPeople] = useState(1)
                
       </Grid>
        
+    </Stack>
+    <Box className='line-L2' width='700px'   display={{xs: 'none', md: 'block'}}>
+                      <img src={Line} alt='' />
+                    </Box>
     </Stack>
     <Footer/>
     </>
