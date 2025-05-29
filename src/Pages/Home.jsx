a516c3c3f354bd55852197759cf7b5aab6d3aa9f
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-05-29 15:03:21 -0300

    up

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
         <AppBar className='reveal home-element-1' color='trasparent' position='static' sx={{display: {xs: 'none', lg: 'block'}, boxShadow: 'none', paddingTop: '3em', paddingLeft: '10em'}} >
            <img src={logo} alt=''/>
         </AppBar>
-        <Box className='reveal home-element' sx={{color: 'white',maxWidth: {lg: '700px'}, padding: {xs: '4em'}, position: 'relative', top: {xs: '4em', sm: '24em',  lg: '1.5em'}, left: {lg: '5.5em'}, textAlign: {xs: 'center', lg: 'left'}}}>
+        <Box className='reveal home-element' sx={{color: 'white',maxWidth: {lg: '700px'}, padding: {xs: '4em'}, position: 'relative', top: {xs: '4em', sm: '20em',  lg: '1.5em'}, left: {lg: '5.5em'}, textAlign: {xs: 'center', lg: 'left'}}}>
            <Typography component='p' margin='1em 0'  display={{xs: 'block', lg: 'none'}}> <Typography component='strong' fontSize={{xs: '2em', sm: '3.5em'}}>dine</Typography>   </Typography>
            <Typography component='h1' fontSize={{xs: '2em', sm: '4em', lg: '5em'}} padding={{xs: '0 1em',sm : '0 2em', lg: '0'}} marginBottom={{xs: '1em' ,lg: '0'}} lineHeight={{xs:'1.2em', lg: '1.3em'}}>Exquisite dining since 1989</Typography>
            <Typography component='p' maxWidth={{lg: '400px'}} margin='1em 0' padding={{sm: '0 13em', lg: '0'}} fontSize={{md: '1.3em'}} lineHeight={{xs: '1.5em', lg: '1.7em'}} >Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from 
