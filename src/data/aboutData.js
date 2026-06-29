191e92525d58a7e4d28e8869bd62dabc2df5a147
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-06-29 13:22:51 -0300

    refactor: creating aboutData.js and communData.js to use images

---
+++ src/data/aboutData.js
@@ -0,0 +1,13 @@
+import EnjoyablePlace1 from "../assets/images/homepage/enjoyable-place-mobile@2x.jpg";
+import EnjoyablePlace2 from "../assets/images/homepage/enjoyable-place-desktop@2x.jpg";
+import LocallySourced1 from "../assets/images/homepage/locally-sourced-mobile@2x.jpg";
+import LocallySourced2 from "../assets/images/homepage/locally-sourced-desktop@2x.jpg";
+
+
+
+export const AboutData = {
+    EnjoyablePlace1,
+    EnjoyablePlace2,
+    LocallySourced1,
+    LocallySourced2
+}
---
+++ src/data/communData.js
@@ -0,0 +1,11 @@
+import Logo from "../assets/images/logo.svg";
+import Plus from '../assets/images/icons/icon-plus.svg'
+import Minus from '../assets/images/icons/icon-minus.svg'
+import Line from "../assets/images/patterns/pattern-lines.svg";
+
+export const communData = {
+    Logo,
+    Plus,
+    Minus,
+    Line
+}
