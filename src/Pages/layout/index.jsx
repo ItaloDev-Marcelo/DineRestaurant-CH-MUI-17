49d368a45ab3f7dcfc1e3a684cd99e97d7de6b3e
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-06-29 14:47:04 -0300

    fix: remove scroolReveal from index.jsx

--- src/Pages/layout/index.jsx
+++ src/Pages/layout/index.jsx
@@ -4,28 +4,7 @@ import MenuSection from '../MenuSection'
 import MiddleSection from '../MiddleSection'
 import BookATableSection from '../BookATableSection'
 import Rodape from '../Footer'
-import ScrollReveal from "scrollreveal";
-import {useEffect} from 'react'
 export default function MainPag()  {
-
-     useEffect(() => {
-        ScrollReveal().reveal(".reveal", {
-          distance: "10px",
-          duration: 1000,
-          easing: "ease-in-out",
-          origin: "left",
-          reset: true, // Se quiser que a animação ocorra toda vez que o elemento entrar na tela
-        });
-
-        ScrollReveal().reveal(".revealR", {
-          distance: "10px",
-          duration: 1000,
-          easing: "ease-in-out",
-          origin: "right",
-          reset: true, // Se quiser que a animação ocorra toda vez que o elemento entrar na tela
-        });
-       }, []) 
-
 
      return (
        <>
