dcddde5d1cf40aedae24764ea46aaf470da90e70
Italo#Dev2000 <devfrontend2028study@gmail.com>
2026-06-29 13:28:28 -0300

    refactor: updating Image component to render 3 img tags

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,3 +1,9 @@
-export default function Img({url}) {
-    return <img src={url} alt="" />
+export default function Img({url1, url2,url3}) {
+    return (
+       <>
+         <img src={url1} alt="" className='md:hidden p-10' />
+         <img src={url2} alt="" className='hidden md:block w-[90%] p-5  xl:hidden'/>
+         <img src={url3} alt="" className='hidden xl:block w-[60%] relative z-100' />
+       </>
+    )
 }
